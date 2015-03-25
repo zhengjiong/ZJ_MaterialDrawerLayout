3440c9612713f2d9eaffe39f3fdd9f3a4e494c7f
zhengjiong <zhengjiong2006@gmail.com>
2015-03-25 23:19:43 +0800

    finish

--- app/src/main/java/com/zj/example/material/drawerlayout/MyActivity.java
+++ app/src/main/java/com/zj/example/material/drawerlayout/MyActivity.java
@@ -1,17 +1,81 @@
 package com.zj.example.material.drawerlayout;
 
-import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+/**
+ * 使用ToolBar代替ActionBar
+ *
+ * create by zhengjiong
+ * Date: 2015-03-25
+ * Time: 22:16
+ */
+public class MyActivity extends ActionBarActivity {
+    private String[] mItems = {"List Item 01", "List Item 02", "List Item 03", "List Item 04"};
 
-public class MyActivity extends Activity {
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mListView;
+
+    private ArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.main_layout);
+
+
+        initView();
+        initToolbar();
+        initData();
+    }
+
+    private void initData() {
+        mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mItems);
+        mListView.setAdapter(mAdapter);
+    }
+
+    private void initToolbar() {
+        mToolbar.setTitle("Toolbar");
+        mToolbar.setTitleTextColor(Color.WHITE);
+
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);//设置返回键可用
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.open, R.string.close){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    private void initView() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mListView = (ListView) findViewById(R.id.listview_menu);
     }
 
 
